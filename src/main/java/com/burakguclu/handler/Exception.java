82b665f2e41935b0771c4b3025f5beb69d641885
Burak Guclu <burak.guclu@tedu.edu.tr>
2025-02-27 21:24:18 +0300

    Catching exception is done, return format is beatufied

---
+++ src/main/java/com/burakguclu/handler/Exception.java
@@ -0,0 +1,20 @@
+package com.burakguclu.handler;
+
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Exception<E> {
+
+	private String hostName;
+	
+	private String path;
+	
+	private Date createTime;
+	
+	private E message;
+	
+}
